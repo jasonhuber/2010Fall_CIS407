13761635d16f7b8edcfb6a82fceb7d6cec20070a
Jason Huber <jasonhuber@gmail.com>
2010-09-01 16:59:38 -0700

    Initial commit

---
+++ Day1/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" trace="true"%>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title></title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox runat="server" ID="txtUserCount"></asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Day1/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        int i = 0;
+
+
+        if (Session["mysessionvar"] != null)
+        {
+            Session["mysessionvar"] = int.Parse(Session["mysessionvar"].ToString()) + 1;
+        }
+        else
+        {
+            Session["mysessionvar"] = 1;
+        }
+        Response.Write("Usercount: ");
+        txtUserCount.Text = Session["mysessionvar"].ToString();
+        
+    }
+}
