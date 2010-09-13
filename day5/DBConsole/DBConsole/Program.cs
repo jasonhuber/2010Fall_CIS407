0f48c93176dbdb15a689964690f998f28581b879
Jason Huber <jasonhuber@gmail.com>
2010-09-13 16:53:50 -0700

    Added day 5 code.

---
+++ day5/DBConsole/DBConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DBConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //get this from connectionstrings.com/access
+            string connstring = 
+                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\DropBox\My Dropbox\Devry\CIS407\SU10B\day5\NorthWind.mdb;";
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+            conn.ConnectionString = connstring;
+            conn.Open();
+            //here I can talk to my db...
+            Console.WriteLine(conn.State);
+            conn.Close();
+            conn = null;
+        }
+    }
+}
