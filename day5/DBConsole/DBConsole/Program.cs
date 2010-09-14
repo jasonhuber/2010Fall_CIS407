1b3b6765e1af10b1ec9487428184d3e61238a126
Jason Huber <jasonhuber@gmail.com>
2010-09-13 17:13:01 -0700

    updated SQL with a try

--- day5/DBConsole/DBConsole/Program.cs
+++ day5/DBConsole/DBConsole/Program.cs
@@ -9,17 +9,30 @@ namespace DBConsole
     class Program
     {
         static void Main(string[] args)
-        {   
-            //get this from connectionstrings.com/access
-            string connstring = 
-                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\DropBox\My Dropbox\Devry\CIS407\SU10B\day5\NorthWind.mdb;";
+        {
+            string connstring =
+    @"Provider=sMicrosoft.Jet.OLEDB.4.0;Data Source=E:\DropBox\My Dropbox\Devry\CIS407\SU10B\day5\NorthWind.mdb;";
             System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
-            conn.ConnectionString = connstring;
-            conn.Open();
-            //here I can talk to my db...
-            Console.WriteLine(conn.State);
-            conn.Close();
-            conn = null;
+
+            try
+            {
+                //get this from connectionstrings.com/access
+                conn.ConnectionString = connstring;
+                conn.Open();
+                //here I can talk to my db...
+
+                Console.WriteLine(conn.State);
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+            finally
+            {
+                conn.Close();
+                conn = null;
+            }
         }
     }
 }
