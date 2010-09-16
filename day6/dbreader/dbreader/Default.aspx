469ec16d9b6017ef25ca7d203487d660dc5e22d2
Jason Huber <jasonhuber@gmail.com>
2010-09-15 17:36:24 -0700

    Added day 6 which is a web version of the dbconsole from day 5

---
+++ day6/dbreader/dbreader/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="dbreader._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        A DB Reader
+    </h2>
+  Connection:  <asp:TextBox ID="txtConnString" runat="server"></asp:TextBox><br />
+  SQL:  <asp:TextBox ID="txtSQL" runat="server"></asp:TextBox><br />
+  <asp:Button ID="btnGo" runat="server" text="Go!" onclick="btnGo_Click" /><br />
+  Results:  <asp:TextBox ID="txtresults" runat="server" Rows="20" 
+        TextMode="MultiLine" Height="112px" Width="542px"></asp:TextBox><br />
+</asp:Content>
---
+++ day6/dbreader/dbreader/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dbreader
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connstring =
+   @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\DropBox\My Dropbox\Devry\CIS407\SU10B\day5\NorthWind.mdb;";
+            txtConnString.Text = connstring;
+        }
+
+        protected void btnGo_Click(object sender, EventArgs e)
+        {
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+            string sql = "";
+            System.Data.DataSet ds = new System.Data.DataSet();
+            System.Data.OleDb.OleDbDataReader dr;
+            System.Data.OleDb.OleDbCommand comm = new System.Data.OleDb.OleDbCommand();
+
+            try
+            {
+                //get this from connectionstrings.com/access
+                conn.ConnectionString = txtConnString.Text;
+                conn.Open();
+                //here I can talk to my db...
+                comm.Connection = conn;
+
+                //Console.WriteLine(conn.State);
+                sql = txtSQL.Text;
+                comm.CommandText = sql;
+                if (sql.ToLower().IndexOf("select") == 0)
+                {
+                    dr = comm.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        txtresults.Text = dr.GetValue(0).ToString();
+                    }
+                }
+                else
+                {
+                    txtresults.Text = comm.ExecuteNonQuery().ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtresults.Text = ex.Message;
+
+            }
+            finally
+            {
+            
+                comm.Dispose();
+                conn.Close();
+                conn = null;
+            }
+        }
+    }
+}
